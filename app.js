aa890270cbe5c9b19cd505b791a817dd388e8525
AshwiniSReddy <128669434+AshwiniSReddy@users.noreply.github.com>
2025-09-23 22:19:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ dotenv.config();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use("/", feedback);
+app.use("/api", feedback);
 
 async function createExcelFile(data, filePath) {
   const workbook = new ExcelJS.Workbook();
